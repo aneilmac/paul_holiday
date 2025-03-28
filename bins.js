355c1f62011fd30278b2b0f9f7c16230f36a5e81
Archibald Neil MacDonald <archibaldnmac@gmail.com>
2025-03-28 13:15:12 +0000

    Tidy up.

--- bins.js
+++ bins.js
@@ -1,18 +1,15 @@
-const BINS_EPOCH = new Date(2025,2,23);
+const BINS_EPOCH = new Date(2025,2,24);
 const MILLISECONDS_PER_WEEK = 1000 * 60 * 60 * 24 * 7;
 
 function diff_weeks(dt2, dt1)  {
-  // Calculate the difference in milliseconds between dt2 and dt1
-  var diff = (dt2 - dt1);
-  return Math.floor(diff / MILLISECONDS_PER_WEEK);
+  return Math.floor((dt2 - dt1) / MILLISECONDS_PER_WEEK);
 }
 
-function is_date_even(dt) {
-    // Is the difference between the current week and the epoch week divisible by 2?
-    return diff_weeks(dt, BINS_EPOCH.getTime()) % 2 === 0;
+function is_week_even(dt) {
+  return diff_weeks(dt, BINS_EPOCH.getTime()) % 2 === 0;
 }
 
-function is_even_week() {
-    return is_date_even(new Date(Date.now()).setHours(0, 0, 0, 0));
+function is_current_week_even() {
+  return is_week_even(Date.now());
 }
 
